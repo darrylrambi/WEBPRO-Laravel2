47cce1d208dcb85b5f8f380eba751c83698cbcd1
darrylrambi <darrylrambi@gmail.com>
2024-12-01 18:14:05 +0700

    completed UI DaftarPegawai dan set routesnya

--- resources/views/DaftarPegawai.blade.php
+++ resources/views/DaftarPegawai.blade.php
@@ -1,3 +1,20 @@
 <div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
+    <title>Daftar Pegawai</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <label for="Nama" class="form-label">Nama</label>
+    <input type="password" id="Nama" name="Nama" class="form-control">
+
+    <label for="Posisi" class="form-label">Posisi</label>
+    <input type="password" id="Posisi" name="Posisi" class="form-control">
+
+    <label for="Gaji" class="form-label">Gaji</label>
+    <input type="password" id="Gaji" name="Gaji" class="form-control">
+
+    <button type="button" class="btn btn-primary">Simpan</button>
+    <button type="button" class="btn btn-secondary">Kembali</button>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </div>
